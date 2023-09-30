c8a429cbc6ddce1ba7625f691044ff1972f1c587
Thabnir <thabtheman@gmail.com>
2023-09-30 16:11:19 -0400

    current app / html version

--- app.py
+++ app.py
@@ -14,11 +14,34 @@ def index():
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     form = request.form
-    user_data = form.to_dict()
+    user_prefs = form.to_dict()
     print("behold, the user data:")
-    print(user_data)
+    print(user_prefs)
 
-    return render_template('tinder.html', user_data=user_data)
+    # do a search function
+    apartment_data = [
+        {
+            'address': '123 Main St',
+            'price': 1000,
+            'bedrooms': 2,
+            'bathrooms': 1,
+            'sqft': 1000,
+            'profile_image_url': 'https://cdn.realtor.ca/listings/TS638316326049630000/reb5/highres/6/22886256_1.jpg',
+            'amenities': ['pool', 'gym', 'parking'],
+        },
+        {
+            'address': '456 Main St',
+            'price': 2000,
+            'bedrooms': 3,
+            'bathrooms': 2,
+            'sqft': 2000,
+            'profile_image_url': 'https://images.unsplash.com/photo-1605146769289-440113cc3d00?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80',
+            'amenities': ['pool', 'no gym sadly :(', 'parking'],
+        }
+    ]
+
+
+    return render_template('tinder.html', apartment_data=apartment_data)
 
 
 if __name__ == '__main__':
