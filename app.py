6a54cb10f37251bf3721552b506d9d27a643de9b
Thabnir <thabtheman@gmail.com>
2023-09-30 15:47:39 -0400

    added some WIP html stuff

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
+Bootstrap(app)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    form = request.form
+    user_data = form.to_dict()
+    print("behold, the user data:")
+    print(user_data)
+
+    return render_template('tinder.html', user_data=user_data)
 
 
 if __name__ == '__main__':
